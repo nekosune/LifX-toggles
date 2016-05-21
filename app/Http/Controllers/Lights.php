9ad25687074b86976ad4b1ad0f3bdc428a7f3e0e
Katrina Swales <kat.swales@nekokittygames.com>
2016-05-21 14:09:47 +0000

    Base

---
+++ app/Http/Controllers/Lights.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace LifX\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use LifX\Http\Requests;
+
+class Lights extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+}
